12d6e85c109078dcda8c28f1083f17333cb7ad9e
Marko <markectodorovic@gmail.com>
2022-06-14 15:17:59 +0200

    nice

---
+++ js/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+    function animation() {
+        let windowHeight = $(window).height();
+        let scroll = $(window).scrollTop();
+        $(".animation").each(function () {
+            let position = $(this).offset().top
+            let animationName = $(this).attr("data-animation");
+            let delay = $(this).attr("data-delay");
+
+            if (position < windowHeight + scroll - 100) {
+                $(this).addClass(animationName);
+                $(this).css("animation-delay", delay);
+            }
+        })
+    }
+    $(window).scroll(function () {
+        animation();
+    })
+    animation();
+
+    $(document).ready(function () {
+        $(".owl-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+
+        });
+    });
+
+});
+
+//contact-page
+if ($(".contact-form").length > 0) {
+    $(".contact-form").validate({
+        highlight: function (element) {
+            $(element).addClass("is-invalid").removeClass("is-valid")
+        },
+        unhighlight: function (element) {
+            $(element).addClass("is-valid").removeClass("is-invalid")
+        },
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+            },
+            email: {
+                required: true,
+                email: true
+            },
+
+            subject: {
+                required: true,
+                minlength: 3,
+            },
+
+            message: {
+                required: true,
+
+            },
+
+        },
+        messages: {
+            name: {
+                required: "The Name* field is required",
+            },
+            email: {
+                required: "The Email* field is required",
+                emails: "Pleas provide valid email address"
+            },
+
+            subject: {
+                required: "The Subject* field is required",
+            },
+            message: {
+                required: "The Message* field is required",
+            }
+        },
+        errorElement: "p",
+        errorPlacement: function (error, element) {
+            error.appendTo(element.closest(".form-group").find(".error-msg"));
+        }
+    });
+}
